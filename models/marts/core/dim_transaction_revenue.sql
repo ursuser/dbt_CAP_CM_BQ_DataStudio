85feef4b0ae40da6b5df7aed637fc6c42ae95c8c
Sergiu Ursu <ursuser@gmail.com>
2023-07-12 10:13:12 +0000

    partitioned tables and incremental models

---
+++ models/marts/core/dim_transaction_revenue.sql
@@ -0,0 +1,37 @@
+with
+    ga4_source as
+
+    (select * from {{ source("analytics_293084740", "events_202*") }}),
+
+    {{
+        config(
+            materialized="table",
+            partition_by={
+                "field": "event_date",
+                "data_type": "date",
+                "granularity": "day",
+            },
+        )
+    }}
+
+    final as
+
+    (
+        select
+            parse_date('%Y%m%d', event_date) as event_date,
+            user_id,
+            platform,
+            (
+                select value.string_value
+                from unnest(event_params)
+                where key = "transaction_id"
+            ) as transaction_id,
+            ecommerce.unique_items as unique_items,
+            ecommerce.total_item_quantity as quantity,
+            round(ecommerce.purchase_revenue, 2) as revenue
+        from ga4_source
+        where event_name = 'purchase'
+    )
+
+select *
+from final
