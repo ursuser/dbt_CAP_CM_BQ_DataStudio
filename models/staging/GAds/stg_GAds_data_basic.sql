bbdb246dc37071a558a1896925a4ebb2c87d8977
Sergiu Ursu <ursuser@gmail.com>
2024-09-17 12:36:59 +0000

    small test

--- models/staging/GAds/stg_GAds_data_basic.sql
+++ models/staging/GAds/stg_GAds_data_basic.sql
@@ -1,9 +1,9 @@
 SELECT
   segments_date,
   campaign_id,
-  SUM(metrics_impressions) AS Impr,
-  SUM(metrics_clicks) AS Clicks,
-  SUM(metrics_cost_micros) / 1000000 AS Cost
+  SUM(metrics_impressions) AS impr,
+  SUM(metrics_clicks) AS clicks,
+  SUM(metrics_cost_micros) / 1000000 AS cost
 FROM
    {{source('google_ads_322_300_5315', 'p_ads_CampaignBasicStats_3223005315')}}
 GROUP BY
